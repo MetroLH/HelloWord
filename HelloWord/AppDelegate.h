2cdd5b425dad551086541c2f3e9cf691990c50fe
刘海 <liuhai@liuhaidembp.lan>
2016-11-17 18:32:34 +0800

    Initial Commit

---
+++ HelloWord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWord
+//
+//  Created by 刘海 on 2016/11/17.
+//  Copyright © 2016年 刘海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWord
+//
+//  Created by 刘海 on 2016/11/17.
+//  Copyright © 2016年 刘海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
